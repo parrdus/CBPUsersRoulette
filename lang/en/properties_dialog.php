d683b55d837fd5ac78b72af3681420d622a085ea
Andrei M. <parrdus@ya.ru>
2026-05-09 17:16:00 +0300

    Create properties_dialog.php

---
+++ lang/en/properties_dialog.php
@@ -0,0 +1,11 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die();
+
+$MESS['BPRU_PD_DEFAULT_USER'] = 'Default User';
+$MESS['BPRU_PD_DEFAULT_USER_HINT'] = 'Assignee to be selected if candidate pool is empty';
+$MESS['BPRU_PD_USERS_LIST'] = 'User IDs to select from';
+$MESS['BPRU_PD_USERS_LIST_HINT'] = 'Format: comma-separated string. E.g.: 12, 45, 67';
+$MESS['BPRU_PD_DEPT_LIST'] = 'Department IDs';
+$MESS['BPRU_PD_DEPT_LIST_HINT'] = 'Where to pull users from. Subdepartments included automatically. E.g.: 3, 7';
+$MESS['BPRU_PD_FORBIDDEN_USERS'] = 'User IDs to exclude';
+$MESS['BPRU_PD_FORBIDDEN_USERS_HINT'] = 'Always excluded from selection. E.g.: 12, 89';
