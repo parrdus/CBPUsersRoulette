3d39a8740077e4f747d766269d8a769eaf49ac5e
Andrei M. <parrdus@ya.ru>
2026-05-09 17:10:24 +0300

    Create .description.php

---
+++ lang/ru/.description.php
@@ -0,0 +1,8 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die();
+
+$MESS['BPRU_ACTIVITY_NAME'] = 'Рулетка пользователей';
+$MESS['BPRU_ACTIVITY_DESCRIPTION'] = 'Выбирает одного случайного пользователя из списка и/или отделов с возможностью исключений';
+$MESS['BPRU_ACTIVITY_CATEGORY_NAME'] = 'Пользовательские активити';
+$MESS['BPRU_ACTIVITY_RETURN_USER'] = 'Выбранный пользователь';
+$MESS['BPRU_ACTIVITY_RETURN_LOG'] = 'Лог выполнения';
