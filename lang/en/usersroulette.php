cef8894457c22850ced9d832afeaf4333e5f4341
Andrei M. <parrdus@ya.ru>
2026-05-09 17:16:50 +0300

    Create usersroulette.php

---
+++ lang/en/usersroulette.php
@@ -0,0 +1,16 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die();
+
+$MESS['BPRU_ERROR_EMPTY_DEFAULT'] = 'DefaultUser is empty, action cannot proceed without fallback user';
+$MESS['BPRU_ERROR_STRUCTURE_NOT_FOUND'] = 'Company structure information block not found (intranet/iblock_structure)';
+$MESS['BPRU_LOG_DEFAULT_USER'] = 'DefaultUser: #DEFAULT_USER#' . PHP_EOL;
+$MESS['BPRU_LOG_EXPLICIT_USERS'] = 'Explicit users: #USERS_LIST#' . PHP_EOL;
+$MESS['BPRU_LOG_DEPT_RESOLVED'] = 'Departments resolved: {#DEPT_IDS#}' . PHP_EOL;
+$MESS['BPRU_LOG_DEPT_USERS'] = 'Department users: {#USERS_LIST#}' . PHP_EOL;
+$MESS['BPRU_LOG_FORBIDDEN_USERS'] = 'Forbidden users: {#USERS_LIST#}' . PHP_EOL;
+$MESS['BPRU_LOG_FINAL_POOL'] = 'Final pool: #USERS_LIST#' . PHP_EOL;
+$MESS['BPRU_LOG_NO_CANDIDATES'] = 'Result: no candidates available' . PHP_EOL;
+$MESS['BPRU_LOG_SELECTED'] = 'Selected: #USER_NAME# {#USER_ID#} from #COUNT# candidates' . PHP_EOL;
+$MESS['BPRU_LOG_FINAL_POOL_FL'] = 'Final pool (#COUNT#): #USERS_LIST#' . PHP_EOL;
+$MESS['BPRU_LOG_WINNER'] = 'Winner: key=#KEY#, user=#USER#' . PHP_EOL;
+$MESS['BPRU_PD_ERROR_EMPTY_DEFAULT'] = 'The Default User is required';
