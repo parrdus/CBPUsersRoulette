e96fa72c0857fd20e9fd35f3b3ac592c94090e6a
Andrei M. <parrdus@ya.ru>
2026-05-09 17:17:24 +0300

    Create properties_dialog.php

---
+++ lang/ru/properties_dialog.php
@@ -0,0 +1,11 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die();
+
+$MESS['BPRU_PD_DEFAULT_USER'] = 'Пользователь по умолчанию';
+$MESS['BPRU_PD_DEFAULT_USER_HINT'] = 'Исполнитель, который будет выбран, если пул кандидатов пуст';
+$MESS['BPRU_PD_USERS_LIST'] = 'Список ID пользователей для выбора';
+$MESS['BPRU_PD_USERS_LIST_HINT'] = 'Формат: строка, разделитель — запятая. Например: 12, 45, 67';
+$MESS['BPRU_PD_DEPT_LIST'] = 'Список ID департаментов';
+$MESS['BPRU_PD_DEPT_LIST_HINT'] = 'Откуда брать пользователей. Автоматически включаются подотделы. Например: 3, 7';
+$MESS['BPRU_PD_FORBIDDEN_USERS'] = 'Список ID пользователей для исключения';
+$MESS['BPRU_PD_FORBIDDEN_USERS_HINT'] = 'Которых всегда исключаем из выбора. Например: 12, 89';
