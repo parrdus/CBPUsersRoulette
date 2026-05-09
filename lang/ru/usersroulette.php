5e6ef93c7b3fb5040a78ae8273dd1b8f9f5f90f3
Andrei M. <parrdus@ya.ru>
2026-05-09 17:18:17 +0300

    Create usersroulette.php

---
+++ lang/ru/usersroulette.php
@@ -0,0 +1,16 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die();
+
+$MESS['BPRU_ERROR_EMPTY_DEFAULT'] = 'Пользователь по умолчанию обязателен, активити не будет выполнен';
+$MESS['BPRU_ERROR_STRUCTURE_NOT_FOUND'] = 'Инфоблок структуры компании не найден (intranet/iblock_structure)';
+$MESS['BPRU_LOG_DEFAULT_USER'] = 'Пользователь по умолчанию: #DEFAULT_USER#' . PHP_EOL;
+$MESS['BPRU_LOG_EXPLICIT_USERS'] = 'Явный список: #USERS_LIST#' . PHP_EOL;
+$MESS['BPRU_LOG_DEPT_RESOLVED'] = 'Департаменты определены: {#DEPT_IDS#}' . PHP_EOL;
+$MESS['BPRU_LOG_DEPT_USERS'] = 'Пользователи из департаментов: {#USERS_LIST#}' . PHP_EOL;
+$MESS['BPRU_LOG_FORBIDDEN_USERS'] = 'Исключённые пользователи: {#USERS_LIST#}' . PHP_EOL;
+$MESS['BPRU_LOG_FINAL_POOL'] = 'Итоговый пул: #USERS_LIST#' . PHP_EOL;
+$MESS['BPRU_LOG_NO_CANDIDATES'] = 'Результат: кандидаты не найдены' . PHP_EOL;
+$MESS['BPRU_LOG_SELECTED'] = 'Выбран: #USER_NAME# {#USER_ID#} из #COUNT# кандидатов' . PHP_EOL;
+$MESS['BPRU_LOG_FINAL_POOL_FL'] = 'Итоговый пул (#COUNT#): #USERS_LIST#' . PHP_EOL;
+$MESS['BPRU_LOG_WINNER'] = 'Победитель: key=#KEY#, user=#USER#' . PHP_EOL;
+$MESS['BPRU_PD_ERROR_EMPTY_DEFAULT'] = 'Пользователь по умолчанию обязателен';
