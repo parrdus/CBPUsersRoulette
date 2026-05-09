99980406937960fc1e7ad471e3bd35a4e3366cd7
Andrei M. <parrdus@ya.ru>
2026-05-09 17:10:58 +0300

    Create .description.php

---
+++ lang/en/.description.php
@@ -0,0 +1,8 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die();
+
+$MESS['BPRU_ACTIVITY_NAME'] = 'User Roulette';
+$MESS['BPRU_ACTIVITY_DESCRIPTION'] = 'Selects one random user from a list and/or departments with exclusion support';
+$MESS['BPRU_ACTIVITY_CATEGORY_NAME'] = 'Custom activity';
+$MESS['BPRU_ACTIVITY_RETURN_USER'] = 'Selected User';
+$MESS['BPRU_ACTIVITY_RETURN_LOG'] = 'Execution Log';
